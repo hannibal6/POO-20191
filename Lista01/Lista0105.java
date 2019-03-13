8d494ad31cc9899296630bba90bdb72727ce6c94
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-13 00:50:59 -0300

    Lista01 Q 05-06

---
+++ Lista01/Lista0105.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class  Lista0105 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int num;
+		num = input.nextInt();
+		
+		if (num == 1) {
+			System.out.println("Janeiro.");
+		}
+		else if (num == 2) {
+			System.out.println("Fevereiro.");
+		}
+		else if (num == 3) {
+			System.out.println("Marco.");
+		}
+		else if (num == 4) {
+			System.out.println("Abril.");
+		}
+		else if (num == 5) {
+			System.out.println("Maio.");
+		}
+		else if (num == 6) {
+			System.out.println("Junho.");
+		}
+		else if (num == 7) {
+			System.out.println("Julho.");
+		}
+		else if (num == 8) {
+			System.out.println("Agosto.");
+		}
+		else if (num == 9) {
+			System.out.println("Setembro.");
+		}
+		else if (num == 10) {
+			System.out.println("Outubro.");
+		}
+		else if (num == 11) {
+			System.out.println("Novembro.");
+		}
+		else if (num == 12) {
+			System.out.println("Dezembro.");
+		}
+	}
+}
