9ea00ac4acd84d306df12f94631998ffc8bb58b8
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-12 22:50:07 -0300

    Lista01 Q 01-02

---
+++ Lista0101.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class  Lista0101 {
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	double velocidade;
+	velocidade = input.nextDouble();
+	
+    System.out.println(velocidade*3.6 + "km/h");
+    }
+}
---
+++ Lista0102.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class  Lista0102 {
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	int anoNascimento;
+	anoNascimento = input.nextInt();
+	
+    System.out.println(2019 - anoNascimento);
+    }
+}
