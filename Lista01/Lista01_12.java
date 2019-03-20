21588cc08addccd8722830036f551a22b25e9110
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-20 00:33:15 -0300

    Lista01 Q 01-12

--- Lista01/Lista01_12.java
+++ Lista01/Lista01_12.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class  Lista01_12 {
 	public static void main(String[] args) {
 		for (int i = 0; i < 11; i++) {
-			System.out.println(i + " " + i*i + " " + i*i*i);		
+			System.out.println(i + " " + i * i + " " + i * i * i);		
 		}
 	}
 }
