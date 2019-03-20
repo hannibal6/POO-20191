37c7d34e1838ab01852221c0fd4553e3dfde09c5
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-20 00:17:01 -0300

    Delete Lista01_12.java

--- Lista01_12.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class  Lista01_12 {
-	public static void main(String[] args) {
-		for (int i = 0; i < 11; i++) {
-			System.out.println(i + " " + i*i + " " + i*i*i);		
-		}
-	}
-}
