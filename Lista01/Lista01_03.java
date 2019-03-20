e32fe5aa690ed4f40425e9dfe31cfee043626b34
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-20 00:23:53 -0300

    Lista01 Q 01-12

--- Lista01/Lista01_03.java
+++ Lista01/Lista01_03.java
@@ -5,10 +5,11 @@ class  Lista01_03 {
 	Scanner input = new Scanner(System.in);
 	
 	double nota1, nota2, nota3;
+	    
 	nota1 = input.nextInt();
 	nota2 = input.nextInt();
 	nota3 = input.nextInt();
 	
-    System.out.println("A media do aluno eh: " + (nota1*2 + nota2*3 + nota3*5)/10);
+    	System.out.println("A media do aluno eh: " + (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10);
     }
 }
