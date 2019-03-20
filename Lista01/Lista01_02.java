c7e70bbeb41f76603cf659d9249c5fd497e3b72a
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-20 00:22:28 -0300

    Lista01 Q 01-12

--- Lista01/Lista01_02.java
+++ Lista01/Lista01_02.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 class  Lista01_02 {
     public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
+    	Scanner input = new Scanner(System.in);
 	
 	int anoNascimento;
 	anoNascimento = input.nextInt();
 	
-    System.out.println("A idade atual eh: " + (2019 - anoNascimento));
+    	System.out.println("A idade atual eh: " + (2019 - anoNascimento));
     }
 }
