13c1215a10250ea92cbbbffd825e40160b8dd0e6
Carlos Vinícius <viniciusccunha@hotmail.com>
2019-03-20 00:17:33 -0300

    Add files via upload

---
+++ Lista01/Lista01_02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class  Lista01_02 {
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	int anoNascimento;
+	anoNascimento = input.nextInt();
+	
+    System.out.println("A idade atual eh: " + (2019 - anoNascimento));
+    }
+}
---
+++ Lista01/Lista01_10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class  Lista01_10 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double num = input.nextDouble();
+		
+		int cont = 0;
+		
+		while (num >= 1) {
+			num /= 2;
+			cont++;
+		}
+		
+		System.out.println("Resultado da ultima divisao: " + num);
+		System.out.println("Numero de divisoes: "  + cont);
+	}
+}
